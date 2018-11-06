a962f1b5bdee6f8c8f893f03fc207f8f95e9e595
Doug Hellmann <doug@doughellmann.com>
2018-11-06 14:44:39 -0500

    class-based dispatcher
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- test1.py
+++ test1.py
@@ -15,103 +15,132 @@ FAKE_EVENTS = [
 ]
 
 # This should come from a configuration option
-NUM_CONCURRENT = 3
+NUM_CONCURRENT = 2
 
 
 async def notification_producer(q):
-    LOG.info('starting notification_producer')
+    log = logging.getLogger('notification_producer')
+    log.info('starting')
     for event in FAKE_EVENTS:
-        LOG.info('sending %s', event)
-        await q.put(event)
+        log.info('NEW EVENT %s', event)
+        await q.put({'project_id': event})
     await q.put(None)
 
 
-_keep_running = True
-_queue_lock = asyncio.Lock()
-_project_queues = {}
 
 
 class ProjectHandler:
+
     def __init__(self, project_id, event_loop):
-        LOG.info('creating handler for %s', project_id)
+        self._log = logging.getLogger('ProjectHandler.{}'.format(project_id))
+        self._log.info('new handler')
         self._project_id = project_id
         self._q = asyncio.Queue()
         self.task = event_loop.create_task(self._consumer(event_loop))
 
     async def _consumer(self, event_loop):
         while True:
-            LOG.info('%s consumer waiting', self._project_id)
+            self._log.debug('consumer waiting')
             event = await self._q.get()
             if event is None:
-                LOG.info('%s consumer stopping', self._project_id)
+                self._log.info('consumer stopping')
                 break
-            LOG.info('%s consumer received %s', self._project_id, event)
+            self._log.info('consumer received %s', event)
+            await asyncio.sleep(0.1)
 
     async def send(self, event):
-        LOG.info('sending %s to %s', event, self._project_id)
+        self._log.info('sending %s', event or '<stop signal>')
         await self._q.put(event)
 
     async def stop(self):
-        LOG.info('sending stop to %s consumer', self._project_id)
         await self.send(None)
 
 
-async def handle_notification(event, event_loop):
-    if not _keep_running:
-        LOG.info('ignoring %s', event)
-        return
-    LOG.info('got %s', event)
-    project_id = event
-    await _queue_lock.acquire()
-    try:
-        if project_id not in _project_queues:
-            _project_queues[project_id] = ProjectHandler(project_id, event_loop)
-        handler = _project_queues[project_id]
-    finally:
-        _queue_lock.release()
-    await handler.send(event)
+class Dispatcher:
+
+    _log = logging.getLogger('Dispatcher')
+
+    def __init__(self, event_loop):
+        self._keep_running = True
+        self._lock = asyncio.Lock()
+        self._handlers = {}
+        self._event_loop = event_loop
+
+    async def dispatch(self, project_id, event):
+        if not self._keep_running:
+            self._log.info('ignoring %s', event)
+            return
+        self._log.info('got %s', event)
+        await self._lock.acquire()
+        try:
+            if project_id not in self._handlers:
+                self._handlers[project_id] = ProjectHandler(project_id,
+                                                          self._event_loop)
+            handler = self._handlers[project_id]
+        finally:
+            self._lock.release()
+        await handler.send(event)
+
+    async def stop(self):
+        await self._lock.acquire()
+        try:
+            self._log.info('stopping all %s handlers', len(self._handlers))
+            self._keep_running = False
+            for project_id, handler in self._handlers.items():
+                await handler.stop()
+        finally:
+            self._lock.release()
+
+    async def wait(self):
+        await self._lock.acquire()
+        try:
+            handler_tasks = [
+                h.task
+                for h in self._handlers.values()
+            ]
+        finally:
+            self._lock.release()
+        self._log.info('waiting for handler tasks to complete')
+        await asyncio.wait(handler_tasks)
 
 
-async def stop_handlers():
-    await _queue_lock.acquire()
-    try:
-        LOG.info('stopping all %s handlers', len(_project_queues))
-        _keep_running = False
-        for project_id, handler in _project_queues.items():
-            await handler.stop()
-    finally:
-        _queue_lock.release()
+async def handle_notification(dispatcher, event):
+    # pretend to get the project_id from the event
+    project_id = event.get('project_id')
+    await dispatcher.dispatch(project_id, event)
 
 
-async def notification_consumer(event_loop, q):
-    LOG.info('starting notification_consumer')
+async def notification_consumer(dispatcher, q):
+    log = logging.getLogger('notification_consumer')
+    log.info('starting')
     while True:
-        LOG.info('notification_consumer waiting')
+        log.debug('waiting')
         event = await q.get()
         if event is None:
-            await stop_handlers()
+            await dispatcher.stop()
             break
-        await handle_notification(event, event_loop)
+        await handle_notification(dispatcher, event)
 
 
 async def main(event_loop):
 
     q = asyncio.Queue(maxsize=NUM_CONCURRENT)
-    consumer_task = event_loop.create_task(notification_consumer(event_loop, q))
-    producer_task = event_loop.create_task(notification_producer(q))
+    dispatcher = Dispatcher(event_loop)
+    consumer_task = event_loop.create_task(
+        notification_consumer(dispatcher, q)
+    )
+    producer_task = event_loop.create_task(
+        notification_producer(q)
+    )
 
     await asyncio.wait([producer_task])
-    await _queue_lock.acquire()
-    try:
-        handler_tasks = [
-            h.task
-            for h in _project_queues.values()
-        ]
-    finally:
-        _queue_lock.release()
-    LOG.info('waiting for handler tasks')
-    await asyncio.wait(handler_tasks)
+    LOG.info('producer finished')
+    LOG.info('waiting for dispatcher to finish')
+    await asyncio.wait([dispatcher.wait()])
+    LOG.info('dispatcher finished')
+    LOG.info('waiting for consumer to finish')
     await asyncio.wait([consumer_task])
+    LOG.info('done')
 
 
 if __name__ == '__main__':
